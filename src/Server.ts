dd2dbdd5d941c94707b14442ecce3d1400c85f79
Andreas Cederström <andreas.cederstrom@factor10.com>
2021-01-28 15:41:12 +0100

    Removed separeat view catalog

--- src/Server.ts
+++ src/Server.ts
@@ -36,12 +36,10 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
  *                              Serve front-end content
  ***********************************************************************************/
 
-const viewsDir = path.join(__dirname, "views");
-app.set("views", viewsDir);
 const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile("index.html", { root: viewsDir });
+  res.sendFile("index.html", { root: staticDir });
 });
 
 // Export express instance
--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,11 +1,3 @@
-import logger from "./Logger";
-
-export const pErr = (err: Error) => {
-  if (err) {
-    logger.err(err);
-  }
-};
-
 export const isSameDate = (a: Date, b: Date) =>
   a.getFullYear() === b.getFullYear() &&
   a.getMonth() === b.getMonth() &&
