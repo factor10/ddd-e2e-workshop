d08929abd3ca11a9efd7871c9d49c5521975b3f0
Andreas Cederström <andreas.cederstrom@factor10.com>
2021-02-02 09:04:17 +0100

    Minor update to test syntax

--- test/e2e/add-registration.spec.js
+++ test/e2e/add-registration.spec.js
@@ -1,34 +1,31 @@
 /// <reference types="cypress" />
 
-context(
-  "Given the consultant Stina Johansson and a day with no registered time",
-  () => {
+context("Given the consultant Stina and a day with no registered time", () => {
+  before(() => {
+    // Will get the fixture from `test/e2e/fixtures/empty-storage.json` and overwrite storage with it
+    cy.fixture("empty-storage").then(testData => {
+      cy.writeFile("src/infrastructure/_storage/e2e-storage.json", testData);
+    });
+    cy.visit("/");
+    cy.get("[cy=select-consultant]").select("Stina Johansson");
+  });
+
+  describe("When registering 200 min spent on Programming @ New app", () => {
     before(() => {
-      // Will get the fixture from `test/e2e/fixtures/empty-storage.json` and overwrite storage with it
-      cy.fixture("empty-storage").then(testData => {
-        cy.writeFile("src/infrastructure/_storage/e2e-storage.json", testData);
-      });
-      cy.visit("/");
-      cy.get("[cy=select-consultant]").select("Stina Johansson");
+      cy.get("[cy=input-date]").type("2021-01-25");
+      cy.get("[cy=select-project]").select("New app");
+      cy.get("[cy=input-activity]").type("Programming");
+      cy.get("[cy=input-duration]").type("200 min");
+      cy.contains("button", "Add registration").click();
     });
 
-    describe("When registering 200 min spent on Programming @ New app", () => {
-      before(() => {
-        cy.get("[cy=input-date]").type("2021-01-25");
-        cy.get("[cy=select-project]").select("New app");
-        cy.get("[cy=input-activity]").type("Programming");
-        cy.get("[cy=input-duration]").type("200 min");
-        cy.contains("button", "Add").click();
-      });
-
-      it("Then the list of registrations shall contain the new registration", () => {
-        cy.get("[cy=registration]").should("have.length", 1);
-        cy.get("[cy=consultant-name]")
-          .first()
-          .should("have.text", "Stina Johansson");
-        cy.get("[cy=project-name]").first().should("have.text", "New app");
-        cy.get("[cy=activity-name]").first().should("have.text", "Programming");
-      });
+    it("Then the list of registrations shall contain the new registration", () => {
+      cy.get("[cy=registration]").should("have.length", 1);
+      cy.get("[cy=consultant-name]")
+        .first()
+        .should("have.text", "Stina Johansson");
+      cy.get("[cy=project-name]").first().should("have.text", "New app");
+      cy.get("[cy=activity-name]").first().should("have.text", "Programming");
     });
-  }
-);
+  });
+});
--- test/e2e/show-existing-registrations.spec.js
+++ test/e2e/show-existing-registrations.spec.js
@@ -1,42 +1,47 @@
 /// <reference types="cypress" />
 
-context("Given a registration for consultant in storage", () => {
-  before(() => {
-    // Will get the fixture from `test/e2e/fixtures/one-registration.json` and overwrite storage with it
-    cy.fixture("one-registration").then(testData => {
-      cy.writeFile("src/infrastructure/_storage/e2e-storage.json", testData);
-    });
-  });
-
-  describe("When visiting site", () => {
-    before(() => cy.visit("/"));
-
-    it("Then should show one day", () => {
-      cy.get("[cy=day]").should("have.length", 1);
+context(
+  "Given a consultant Bruce has registered 200 min Programming @ New app",
+  () => {
+    before(() => {
+      // Will get the fixture from `test/e2e/fixtures/one-registration.json` and overwrite storage with it
+      cy.fixture("one-registration").then(testData => {
+        cy.writeFile("src/infrastructure/_storage/e2e-storage.json", testData);
+      });
     });
 
-    it("Then should show one registration", () => {
-      cy.get("[cy=registration]").should("have.length", 1);
-    });
+    describe("When visiting site", () => {
+      before(() => cy.visit("/"));
 
-    it("Then should show consultants name for the registration", () => {
-      cy.get("[cy=consultant-name]").first().should("have.text", "Bruce Wayne");
-    });
+      it("Then should show one day", () => {
+        cy.get("[cy=day]").should("have.length", 1);
+      });
 
-    it("Then should show project name for the registration", () => {
-      cy.get("[cy=project-name]").first().should("have.text", "New app");
-    });
+      it("Then should show one registration", () => {
+        cy.get("[cy=registration]").should("have.length", 1);
+      });
 
-    it("Then should show activity name for the registration", () => {
-      cy.get("[cy=activity-name]").first().should("have.text", "Programming");
-    });
+      it("Then should show consultants name for the registration", () => {
+        cy.get("[cy=consultant-name]")
+          .first()
+          .should("have.text", "Bruce Wayne");
+      });
 
-    it("Then should show duration for the registration", () => {
-      cy.get("[cy=duration]").first().should("have.text", "200 minutes");
-    });
+      it("Then should show project name for the registration", () => {
+        cy.get("[cy=project-name]").first().should("have.text", "New app");
+      });
 
-    it("Then total registered time for the day should be 200 min", () => {
-      cy.get("[cy=total-duration]").should("have.text", "200 minutes");
+      it("Then should show activity name for the registration", () => {
+        cy.get("[cy=activity-name]").first().should("have.text", "Programming");
+      });
+
+      it("Then should show duration for the registration", () => {
+        cy.get("[cy=duration]").first().should("have.text", "200 minutes");
+      });
+
+      it("Then total registered time for the day should be 200 min", () => {
+        cy.get("[cy=total-duration]").should("have.text", "200 minutes");
+      });
     });
-  });
-});
+  }
+);
